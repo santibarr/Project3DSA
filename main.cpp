5faa4c92516e9bcbc17c3eef124ae8fdbaf7466d
tphan9 <71912485+tphan9@users.noreply.github.com>
2022-12-05 14:46:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// DeleteMe1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,7 +17,11 @@
 using namespace std;
 
 class EnergyProject {
+private:
     void ReadFile();
+
+public:
+
 };
 
 void EnergyProject::ReadFile() {
@@ -25,12 +35,19 @@ void EnergyProject::ReadFile() {
                 firstLine = false;
                 continue;
             }
+
+            //should these be vector<string>?
             string stateYear = "";
+            string state = "";
+            string year = "";
+
             getline(parsedline, line, ','); // state
             stateYear += line;
-            stateYear += "-";
+            state += line; //"Hawaii"
+            stateYear += "-"; //"Hawaii-"
             getline(parsedline, line, ','); // year column
-            stateYear += line;
+            stateYear += line; //"Hawaii-1990"
+            year += line; //"1990"
             getline(parsedline, line, ','); // column c
             getline(parsedline, line, ',');
             getline(parsedline, line, ',');
@@ -75,3 +92,51 @@ void EnergyProject::ReadFile() {
         }
     }
 }
+
+int main() 
+{
+{
+    int choice = 0; 
+    
+    cout << "===================================================== \n";
+    cout << " \t\tMENU \t \n ";
+    cout << "===================================================== \n";
+    cout << " 1. Play the Bad Energy Guessing Game \n"; //which state has the highest nonrenewable energy (bad energy) consumption in 1990?
+    cout << " 2. Play the Good Energy Guessing Game \n";
+    cout << " 3. Find Top 10 States with Highest Nonrenewable Energy Consumption \n";
+    cout << " 4. Find Top 10 States with Lowest Nonrenewable Energy Consumption  \n";
+    cout << " 5. Exit \n";
+
+    // call functions
+    cin >> choice;
+    switch (choice) 
+    {
+        case '1':
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        default:
+            cout << "Error! Invalid choice. Pick a number from 1-5";
+            break;
+        }
+    }
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
